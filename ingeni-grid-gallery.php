5b9570916b38d78b31e9fbafbdfc35f290ff83c4
BL Developer <adrian@dev-2.local>
2020-05-11 16:41:47 +1000

    initial release

---
+++ ingeni-grid-gallery.php
@@ -0,0 +1,120 @@
+<?php
+/*
+Plugin Name: Ingeni Grid Carousel
+Version: 2020.01
+Plugin URI: http://ingeni.net
+Author: Bruce McKinnon - ingeni.net
+Author URI: http://ingeni.net
+Description: Replaces standard Wordpress post galleries with Foundation Grid galleries.
+*/
+
+/*
+Copyright (c) 2019 Ingeni Web Solutions
+Released under the GPL license
+http://www.gnu.org/licenses/gpl.txt
+
+Disclaimer: 
+	Use at your own risk. No warranty expressed or implied is provided.
+	This program is free software; you can redistribute it and/or modify 
+	it under the terms of the GNU General Public License as published by 
+	the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ 	See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+Requires : Wordpress 3.x or newer ,PHP 5 +
+
+v2020.01 - Initial version, based on Ingeni grid Gallery v2019.04
+
+*/
+
+
+//Init hook
+add_action('init', 'grid_override_wp_gallery');
+ 
+//Override function
+function grid_override_wp_gallery()
+{
+    remove_shortcode('gallery');
+    add_shortcode('gallery', 'ingeni_grid_gallery_shortcode');
+}
+ 
+//Custom gallery shortcode
+function ingeni_grid_gallery_shortcode($atts, $content) {
+	$retHtml = '';
+
+	$params = shortcode_atts( array(
+		'ids' => '',
+		'orderby' => 'post__in',
+		'columns' => '2',
+		'container_class' => '',
+		'link' => 'file', //file | link | <empty string> (for linking to attachment page)
+		'close_grid_container' => 1
+	), $atts );
+
+
+	if ( $params['ids'] ) {
+		if ($params['close_grid_container'] > 0) {
+			$retHtml = '</div></div></div>';
+		}
+		$retHtml .= '<div class="grid-container full '.$params['container_class'].' grid-wrap"><div class="grid-x grid-margin-x">';
+
+		$imgIds = explode(",",$params['ids']);
+
+		foreach( $imgIds as $imgId ) {
+			$img_url = wp_get_attachment_image_src( $imgId, "large" );
+			if ($img_url !== false) {
+				$retHtml .= '<div class="cell small-12 medium-6">';
+				$retHtml .= '<div class="photo_grid_item" style="background-image: url('.$img_url[0].');"></div>';
+				$retHtml .= '</div>';
+			}
+		}
+		
+		$retHtml .= '</div></div></div>';
+
+		
+		if ($params['close_grid_container'] > 0) {
+			$retHtml .= '</div></div></div>';
+		}
+	}
+
+	return $retHtml;
+}
+
+
+
+function ingeni_load_grid() {
+	$dir = plugins_url( '', __FILE__ );
+
+	//grid gallery
+	if ( is_singular( 'post' ) || is_singular( 'page' ) ) {
+		wp_enqueue_style( 'ingeni-grid-css', $dir . '/ingeni-grid-gallery.css' );
+	}
+		
+		
+	// Init auto-update from GitHub repo
+	require 'plugin-update-checker/plugin-update-checker.php';
+	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+		'https://github.com/BruceMcKinnon/ingeni-grid-gallery',
+		__FILE__,
+		'ingeni-grid-gallery'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'ingeni_load_grid' );
+
+
+// Plugin activation/deactivation hooks
+function ingeni_grid_activation() {
+	flush_rewrite_rules( false );
+}
+register_activation_hook(__FILE__, 'ingeni_grid_activation');
+
+function ingeni_grid_deactivation() {
+  flush_rewrite_rules( false );
+}
+register_deactivation_hook( __FILE__, 'ingeni_grid_deactivation' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Ingeni Grid Gallery ===
+
+Contributors: Bruce McKinnon
+Tags: gallery, masonry
+Requires at least: 4.8
+Tested up to: 5.1.1
+Stable tag: 2020.01
+
+Replaces standard Wordpress post galleries with Grid galleries.
+
+Based on Ingeni Masonry 
+
+
+
+== Description ==
+
+* - Replaces the standard Wordpress photo gallery with a masonry-style gallery
+
+* - Based on https://masonry.desandro.com by David DeSandro.
+
+
+
+
+== Installation ==
+
+1. Upload the 'ingeni-grid-gallery' folder to the '/wp-content/plugins/' directory.
+
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+3. Display the gallery using the standard Gallery shortcode within Wordpress.
+
+
+
+== Frequently Asked Questions ==
+
+
+
+= How do a display the gallery? =
+
+Use the stock-standard Wordpress gallery shortcode.
+
+The following parameters may be included:
+
+
+ids: Comma-separated list of image ids.
+
+container_class: Extra class that is applied to the container class.
+
+
+
+== Changelog ==
+
+v2019.01 - Initial version based on Ingeni Masonry Grid v2019.04
